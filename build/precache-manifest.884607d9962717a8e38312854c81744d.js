12fc672b407e4b2f2589322f830887dd35110d6e
HariChidam <hchidam@umich.edu>
2023-01-03 12:58:54 -0600

    build

---
+++ build/precache-manifest.884607d9962717a8e38312854c81744d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "c66d063b1170040570a8",
+    "url": "/HariChidam.github.io/static/css/main.354a5acf.chunk.css"
+  },
+  {
+    "revision": "c66d063b1170040570a8",
+    "url": "/HariChidam.github.io/static/js/main.c66d063b.chunk.js"
+  },
+  {
+    "revision": "793cbffc18929eb6de9a",
+    "url": "/HariChidam.github.io/static/js/1.793cbffc.chunk.js"
+  },
+  {
+    "revision": "1d7bcf38bac902ab05f2",
+    "url": "/HariChidam.github.io/static/js/runtime~main.1d7bcf38.js"
+  },
+  {
+    "revision": "f449cf3f60734839d49a8468166e1a84",
+    "url": "/HariChidam.github.io/static/media/profession.f449cf3f.jpg"
+  },
+  {
+    "revision": "27af3fd69608bf2975612ef0ff89cd93",
+    "url": "/HariChidam.github.io/static/media/EmmaForesi.27af3fd6.jpeg"
+  },
+  {
+    "revision": "c03f3410cc70592a95602238a5e91088",
+    "url": "/HariChidam.github.io/static/media/tht logo.c03f3410.png"
+  },
+  {
+    "revision": "4f4bc3aef2c54505aa040d399c8ed6b8",
+    "url": "/HariChidam.github.io/static/media/KatieBailey.4f4bc3ae.jpeg"
+  },
+  {
+    "revision": "038631c53636072b89ca5eda5db64a5e",
+    "url": "/HariChidam.github.io/static/media/Hari.038631c5.JPG"
+  },
+  {
+    "revision": "fa7f17b3aca210c3abdcee7cf949ccc7",
+    "url": "/HariChidam.github.io/static/media/Casey.fa7f17b3.jpeg"
+  },
+  {
+    "revision": "6e0d299f1c15477a5860acd237ca4adc",
+    "url": "/HariChidam.github.io/static/media/Charlie.6e0d299f.jpg"
+  },
+  {
+    "revision": "d6c708afcc5f744fa68e1f79bebb07f0",
+    "url": "/HariChidam.github.io/static/media/instaIcon.d6c708af.png"
+  },
+  {
+    "revision": "f4df3e76f1b12e0a8527025e6afb1386",
+    "url": "/HariChidam.github.io/static/media/PTR.f4df3e76.jpg"
+  },
+  {
+    "revision": "4466c15e0349f53af953b9931554603b",
+    "url": "/HariChidam.github.io/static/media/service.4466c15e.jpg"
+  },
+  {
+    "revision": "7537add8a5bb07deb1cfd8e936b62dd0",
+    "url": "/HariChidam.github.io/static/media/brohood.7537add8.jpeg"
+  },
+  {
+    "revision": "478988a898095ce413c4713145481279",
+    "url": "/HariChidam.github.io/static/media/DEI.478988a8.png"
+  },
+  {
+    "revision": "ca9b04fcd39566eb12fec3d7d7ed62c6",
+    "url": "/HariChidam.github.io/static/media/Retreat.ca9b04fc.JPG"
+  },
+  {
+    "revision": "6dc87d843c87134666b0bd7daeeaec4e",
+    "url": "/HariChidam.github.io/static/media/ProfessionalGroupPic.6dc87d84.jpg"
+  },
+  {
+    "revision": "66b60c217973a700a53015498250e183",
+    "url": "/HariChidam.github.io/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/THT-Website-Hari/precache-manifest.31c06b56daf91d459dac6eac664d843c.js"
+  "/HariChidam.github.io/precache-manifest.884607d9962717a8e38312854c81744d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/THT-Website-Hari/index.html", {
+workbox.routing.registerNavigationRoute("/HariChidam.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
